f0ff38620b4dff1d8484c832d35fae1c223976c9
devchea <rchea817@gmail.com>
2020-08-08 23:54:24 -0700

    added header and routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
 import Header from './Header'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <h1> Tinder Clone</h1>
-      <Header/>
+      <Router>
+        <Switch>
+          <Route path="/chat">
+            <h1>Welcome to the chat</h1>
+          </Route>
+          <Route path="/">
+            <h1>Welcome to the homepage</h1>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   )
 }
--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import "./Header.css"
+import PersonIcon from '@material-ui/icons/Person';
+import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
+import Logo from './love_swipe_logo.png';
+import { makeStyles, IconButton } from '@material-ui/core';
 
-function Header() {
+
+const useStyles = makeStyles(theme => ({
+    icon: {
+      fontSize: '80px'
+    },
+    header: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        borderBottom: '1px solid #f9f9f9',
+        padding: '15px',
+        alignItems: 'center'
+    }
+}));
+
+const Header = () => {
+    const classes = useStyles();
+
     return (
-        <div>
-            <h2>Header</h2>
+        <div className={classes.header}>
+            <IconButton>
+                <PersonIcon className={classes.icon}/>
+            </IconButton>
+            <img 
+                className='header__logo'
+                src={Logo} 
+                alt='love swipe logo' 
+            />
+            <IconButton>
+                <QuestionAnswerIcon className={classes.icon}/>
+            </IconButton>
         </div>
     )
 }
